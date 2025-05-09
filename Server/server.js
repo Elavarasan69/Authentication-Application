bb5d57bff9aa38657bc2f525bee8f626c3595ed4
Elavarasan.t <95843820+Elavarasan69@users.noreply.github.com>
2025-05-09 12:10:44 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -7,14 +7,16 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const port = process.env.PORT || 4000
+
 const authRoutes = require('./routes/authentication')
 app.use('/api',authRoutes)
 
 mongoose.connect(process.env.DATABASE_URI)
     .then(()=>{
         console.log("Database Connected")
-        app.listen(process.env.PORT,()=>{
-            console.log(`Server Connected on http://localhost/${process.env.PORT}`)
+        app.listen(port,()=>{
+            console.log(`Server Connected on http://localhost/${port}`)
         })
     })
     .catch(error => console.log("Error connecting to MongoDB:",error.message))
